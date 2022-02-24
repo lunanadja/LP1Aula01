ecd2185c84d05c18534ee1332416c5d2c636f134
lunanadja <a22104596@alunos.ulht.pt>
2022-02-24 12:36:11 +0100

    Adicionamos comentario ao codigo

---
+++ OlaMundo/Comentario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    // comentario
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World");
+        }
+    }
+}
